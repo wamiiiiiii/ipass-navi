8c17a1cdcb952d6661cf69777b1f125af5175449
wamiiiiiii <wamirere@gmail.com>
2026-04-11 19:17:51 +0900

    fix: SWキャッシュv4

--- sw.js
+++ sw.js
@@ -9,8 +9,8 @@
  */
 
 // キャッシュの名前（バージョンを上げると古いキャッシュを削除できる）
-const CACHE_NAME = 'ipass-navi-v3';
-const DATA_CACHE_NAME = 'ipass-navi-data-v3';
+const CACHE_NAME = 'ipass-navi-v4';
+const DATA_CACHE_NAME = 'ipass-navi-data-v4';
 
 // アプリシェル（UIリソース）：初回インストール時にキャッシュするファイルリスト
 const APP_SHELL_FILES = [
